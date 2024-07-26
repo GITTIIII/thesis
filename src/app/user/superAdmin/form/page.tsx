69d34f7d390384b4040bd46980d3859d415ebae6
Paultan51 <142047948+Paultan51@users.noreply.github.com>
2024-07-26 22:18:59 +0700

    update

--- src/app/user/superAdmin/form/page.tsx
+++ src/app/user/superAdmin/form/page.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+import { Payment, columns } from "./columns";
+import { DataTable } from "./dataTable";
 
-export default function FormDashbord() {
-  return <div>FormDashbord</div>;
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "manazone@example.com",
+    },
+    {
+      id: "3409vsdflg",
+      amount: 3000,
+      status: "pending",
+      email: "pluton@example.com",
+    },
+    {
+      id: "qdqwj234",
+      amount: 4500,
+      status: "pending",
+      email: "pungtonk@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "manazone@example.com",
+    },
+    {
+      id: "3409vsdflg",
+      amount: 3000,
+      status: "pending",
+      email: "pluton@example.com",
+    },
+    {
+      id: "qdqwj234",
+      amount: 4500,
+      status: "pending",
+      email: "pungtonk@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "manazone@example.com",
+    },
+    {
+      id: "3409vsdflg",
+      amount: 3000,
+      status: "pending",
+      email: "pluton@example.com",
+    },
+    {
+      id: "qdqwj234",
+      amount: 4500,
+      status: "pending",
+      email: "pungtonk@example.com",
+    },
+  ];
+}
+
+export default async function FormDashborad() {
+  const data = await getData();
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
--- src/app/user/superAdmin/page.tsx
+++ src/app/user/superAdmin/page.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import axios from "axios";
+import { IComprehensiveExamCommitteeForm } from "@/interface/form";
 
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+async function get01Form(): Promise<IComprehensiveExamCommitteeForm[]> {
+  const res = await axios.get(`/api/01ComprehensiveExamCommitteeForm`);
+  if (res.status != 200) {
+    console.error("Error fetching data:", res.status);
+    throw new Error("Cannot fetch data!");
+  } else {
+    return res.data as IComprehensiveExamCommitteeForm[];
+  }
+}
+
+export default async function Dashboard() {
+  try {
+    const demo = await get01Form();
+    return (
+      <div>
+        {demo.map((data: IComprehensiveExamCommitteeForm) => (
+          <div key={data.id}>
+            <div>{data.committeeName1}</div>
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error rendering component:", error);
+    return <div>Error rendering component!</div>;
+  }
 }
